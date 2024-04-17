30d3bed22a6864eb1bd256c472c9e626ac9d66ef
liuronggang <ronggang.liu@gientech.com>
2024-04-17 11:00:18 +0800

    fix: 提取主题公共部分

--- src/styles/darkTheme.ts
+++ src/styles/darkTheme.ts
@@ -1,8 +1,10 @@
 import { ThemeConfig, theme } from 'antd';
-
+import defaultStyle from './defaultStyle';
+const { token, components } = defaultStyle;
 export default {
   algorithm: theme.defaultAlgorithm,
   token: {
+    ...token,
     fontFamily: 'PingFangSC, 微软雅黑, Helvetic',
     colorSuccess: '#15ad2c',
     colorPrimary: '#1d70f5',
@@ -16,41 +18,15 @@ export default {
     colorBgContainer: 'black',
     colorBgBase: 'black',
     colorBgLayout: 'black',
-    fontSizeHeading4: 18,
-    fontSizeXL: 18,
-    sizeStep: 5,
-    sizeUnit: 4,
-    borderRadius: 4,
-    padding: 16,
-    borderRadiusLG: 4,
   },
   components: {
-    Typography: {
-      fontFamilyCode: "'PingFangSC',Aral,Helvetica",
-      marginXS: 0,
-      marginXXS: 0,
-      paddingSM: 0,
-      titleMarginBottom: '0',
-      titleMarginTop: '0',
-    },
+    ...components,
     Layout: {
+      ...components?.Layout,
       headerColor: '#fff',
       headerBg: 'rgb(4, 51, 107)',
-      headerHeight: 60,
-      headerPadding: '0px 16px',
-      footerPadding: '0',
       triggerBg: 'black',
       triggerColor: 'rgb(4, 51, 107)',
-    },
-    Checkbox: {
-      controlInteractiveSize: 14,
-      fontSize: 14,
-    },
-    Radio: {
-      radioSize: 14,
-    },
-    Tabs: {
-      titleFontSize: 16,
     },
   },
 } as ThemeConfig;
--- src/styles/defaultStyle.ts
+++ src/styles/defaultStyle.ts
@@ -1,2 +1,30 @@
-import { theme } from 'antd';
-export default theme ? {} : {};
+import { ThemeConfig } from 'antd';
+
+export default {
+  token: {
+    fontSizeHeading4: 18,
+    fontSizeXL: 18,
+    sizeStep: 5,
+    sizeUnit: 4,
+    borderRadius: 4,
+    padding: 16,
+    borderRadiusLG: 4,
+  },
+  components: {
+    Layout: {
+      headerHeight: 60,
+      headerPadding: '0px 16px',
+      footerPadding: '0',
+    },
+    Checkbox: {
+      controlInteractiveSize: 14,
+      fontSize: 14,
+    },
+    Radio: {
+      radioSize: 14,
+    },
+    Tabs: {
+      titleFontSize: 16,
+    },
+  },
+} as ThemeConfig;
